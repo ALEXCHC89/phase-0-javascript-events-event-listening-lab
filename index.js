bf5f9bdff64d83b74544eb968efc07deac98a563
Alexon <alexchc89@gmail.com>
2021-03-22 00:59:04 -0700

    Done with assignment (javascript events event listening lab)

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//const input = document.getElementById('input');
+//input.addEventListener('click', function() {
+//    alert('I was clicked!');
+//});
+//function addingEventListener() {
+//    input.addEventListener('click', function() {
+  //      alert('I was clicked!');
+    //})
+//}
+
+const input = document.getElementById('input');
+function clickAlert() {
+  alert('I was clicked!');
+}
+input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
